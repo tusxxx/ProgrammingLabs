ba69ec81f4d3c910afe20b4e49bc4d61bf8ecec4
dmc_ifn <dmc_ifn@mail.ru>
2022-02-07 11:07:42 +0700

    Task 2
    Work begin

---
+++ ProgrammingLabs/Task2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLabs
+{
+    class Document
+    {
+        private string Name;
+        private string Author;
+        private string Tags;
+        private string Genre;
+        private string Location;
+
+        public virtual void GetInfo() { }
+    }
+
+    class MSWord : Document
+    {
+
+    }
+
+    class PDF : Document
+    {
+
+    }
+
+    class MSExcel : Document
+    {
+
+    }
+
+    class TXT : Document
+    {
+
+    }
+
+    class HTML : Document
+    {
+
+    }
+}
