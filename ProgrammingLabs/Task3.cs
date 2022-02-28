9fc68c359221658b623543b59642c4b3be8c4d4b
dmc_ifn <dmc_ifn@mail.ru>
2022-02-28 11:05:11 +0700

    Task3

---
+++ ProgrammingLabs/Task3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingLabs
+{
+    class SquareMatrix: IComparable
+    {
+        private int[][] _Matrix;
+        public SquareMatrix(int[][] SquareArray)
+        {
+            _Matrix = SquareArray;
+        }
+
+
+        public static int operator /(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix) {
+            
+        }
+
+        public static int operator *(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix) {
+            
+        }
+
+        public static int operator +(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix) {
+            
+        }
+
+        public static int operator -(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix) {
+            
+        }
+        
+        public static int operator -(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix) {
+            
+        }
+        
+        public static int operator ==(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix) {
+            
+        }
+        
+        public static int operator !=(SquareMatrix FirstMatrix, SquareMatrix SecondMatrix) {
+            
+        }
+
+        public override bool Equals(object other)
+        {
+            
+        }
+
+        public override int GetHashCode()
+        {
+
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
