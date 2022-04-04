68da185ef9df4d6039032c486a47a5c05c358391
dmc_ifn <dmc_ifn@mail.ru>
2022-04-04 10:18:48 +0700

    Task3

--- ProgrammingLabs/Start.cs
+++ ProgrammingLabs/Start.cs
@@ -14,14 +14,15 @@ namespace ProgrammingLabs
 
             float result = Matrix1 * Matrix2;
             Console.WriteLine(result);
-
-            Caretaker Caretaker1 = new Caretaker();
-            Caretaker1.SaveState(Matrix1);
-            
-
-            result = Matrix1 * Matrix2;
+            result = Matrix1 - Matrix2;
             Console.WriteLine(result);
 
+            ShallowCloneMatrix ClonedMatrix = new ShallowCloneMatrix();
+            ClonedMatrix._Determinant = 2;
+            ShallowCloneMatrix ClonedMatrix2 = (ShallowCloneMatrix)ClonedMatrix.Clone();
+            ClonedMatrix._Determinant = 1;
+            Console.WriteLine(ClonedMatrix._Determinant);
+            Console.WriteLine(ClonedMatrix2._Determinant);
 
         }
     }
